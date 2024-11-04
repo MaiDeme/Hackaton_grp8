b530f54a9dc0f9bb7cbc39730a950119b7c191c7
Beigeaud <anne.beigeaud2@gmail.com>
2024-11-04 11:48:38 +0100

    Correction FeatureCounts : les tests passent

--- run.sh
+++ run.sh
@@ -1,14 +1,24 @@
 #!/bin/bash 
 
-conda activate snakemake
-conda install -y singularity
 
-export PATH=$PATH:/var/lib/miniforge/envs/snakemake/bin/singularity
+#First we install the a recent version of Singularity
+sudo -E env "PATH=$PATH" conda install -y -c conda-forge singularity=3.8.6
 
+#export PATH=$PATH:/var/lib/miniforge/envs/snakemake/bin/singularity
+
+#Then we build the featureCounts image
 if [ ! -f featureCounts.img ]; then
     sudo -E env "PATH=$PATH" singularity build featureCounts.img Singularity.featureCounts
 fi
 
 
+#Finally we execute the Snakfile
 snakemake -s Snakefile --forceall --use-singularity --cores all
+
+
+
+
+
+
+
 
