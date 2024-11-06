2bf58e45de62d0dc70c88d4df0649f837527faa4
TitouanCh <titouan85000@gmail.com>
2024-11-06 23:56:40 +0100

    deleted unevessary

--- genomeref.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wget -O reference.gff "https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?db=nuccore&report=gff3&id=CP000253.1"
-wget "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=CP000253.1&rettype=fasta&retmode=text" -O full-genome.fasta
-
-# Installing bowtie: conda install bowtie
-# 
-
-bowtie-build full-genome.fasta reference.gff
