791a49cbfa074bbbc541370f27fcfa896866f063
Beigeaud <anne.beigeaud2@gmail.com>
2024-12-01 15:28:15 +0100

    Ajout Volcano plot

--- DESeq2.R
+++ DESeq2.R
@@ -2,39 +2,58 @@
 ### Preparation des donnees et des parametres
 ################################################################################
 
+#Nettoyage de la session
 rm(list=objects())
 graphics.off()
 set.seed(12345)
+
+#Chargement des librairies
 
 library(DESeq2)
 library(ggplot2)
 library(EnrichmentBrowser)
 
+#Mise en place du repertoire de travail
 args <- commandArgs(trailingOnly = TRUE)
-
-
 dir = args[1]
-print(dir)
-setwd(dir)
+#dir = "/home/marmotte/Documents/Université/Master BIBS/M2AMI2B/ReproHackathon/Hackaton_grp8"
 
+#Creation des fichiers de sortie
 
-counts = read.table("./results/counts/Combined_table.txt", header = T, row.names = 1)
-#On a le bon nombre de gènes 
+res_dir = paste0(dir,"/results/DESeq2/")
+MAplot_all = paste0(dir,"/results/DESeq2/MA_plot_all.pdf")
+Volcano_plot_all = paste0(dir,"/results/DESeq2/Volcano_plot_all.pdf")
+res_txt = paste0(dir,"/results/DESeq2/DESeq2_results.txt")
+
+if (!dir.exists(res_dir)) {
+  dir.create(res_dir)
+}
+if (!file.exists(MAplot_all)) {
+  file.create(MAplot_all)
+}
+if (!file.exists(Volcano_plot_all)) {
+  file.create(Volcano_plot_all)
+}
+if (!file.exists(res_txt)) {
+  file.create(res_txt)
+}
+
+#Chargement des fichiers 
+counts = read.table(paste0(dir,"/results/counts/Combined_table.txt"), header = T, row.names = 1)
 
 
 ################################################################################
 ####DESeq2
 ################################################################################
 
-#Vecteur de condition : Antibio ou pas 
-#O : controle : pas antibio
-#1 : Antibio 
-#3 Replicats pour chacun 
-help("DESeq2")
-cond <- factor(rep(0:1, each=3))
-head(cond)
+#Vecteur de condition : Antibio ou pas
+#O : controle : 3 derniers
+#1 : Antibio : 3 premiers
 
-#Parametres par defaut sont utilises dans l article et ajustement des pvalues automatique
+cond <- factor(rep(c(1,0), each=3))
+
+
+#Utilisation des parametres par defaut
 dds <- DESeqDataSetFromMatrix(counts, DataFrame(cond), ~ cond)
 dds <- DESeq(dds)  
 res <- results(dds, test = "Wald")
@@ -45,38 +64,85 @@ pvalues = res$padj
 genes_diff = dim(counts[which(pvalues<0.05),])[1]
 genes_up = dim(counts[which(pvalues<0.05 & res$log2FoldChange>0),])[1]
 genes_down = dim(counts[which(pvalues<0.05 & res$log2FoldChange<0),])[1]
-# On remarque qu'on a 1484 genes significatifs
-genes_up + genes_down
-#Normalement : 1477 avec 710 up et 767 down 
+
 
 ################################################################################
-####MA plots
+#### MA plots
 ################################################################################
 
-pdf(pasteo(dir,"/results/Figures/MA_plot_all.pdf"), width = 7, height = 5)
+pdf(MAplot_all)
 
-# Centrer y autour de sa moyenne
-y <- res$log2FoldChange
-y <- y - mean(y, na.rm = TRUE)
 
-boxplot(y)
 
+y <- pmin(pmax(res$log2FoldChange, -4), 4)
 x = apply(X = counts, MARGIN = 1, FUN = mean)
-x2 = log2(apply(X = counts, MARGIN = 1, FUN = mean))
 
-ggplot(mapping = aes(x = x, 
-                     y = y, 
-                     color = ifelse(!is.na(pvalues) & pvalues > 0.05, "Non Significatif", "Significatif"))) +
-  geom_point(size = 1) +
+
+is_max <- res$log2FoldChange > 4 | res$log2FoldChange < -4
+
+# Création des data.frames pour ggplot
+data_normal <- data.frame(x = x[!is_max], 
+                          y = y[!is_max], 
+                          pvalues = pvalues[!is_max])
+data_max <- data.frame(x = x[is_max], 
+                           y = y[is_max], 
+                           pvalues = pvalues[is_max])
+
+
+ggplot() +
+  # Points normaux
+  geom_point(data = data_normal,
+             mapping = aes(x = x, 
+                           y = y, 
+                           color = ifelse(!is.na(pvalues) & pvalues > 0.05, 
+                                          "Non Significatif", 
+                                          "Significatif")),
+             size = 1) +
+  # Triangles pour les valeurs limites
+  geom_point(data = data_max,
+             mapping = aes(x = x, 
+                           y = y, 
+                           color = ifelse(!is.na(pvalues) & pvalues > 0.05, 
+                                          "Non Significatif", 
+                                          "Significatif")),
+             shape = 17,  
+             size = 1) + 
+  # Couleurs
   scale_color_manual(values = c("Non Significatif" = "black", "Significatif" = "red"),
                      name = NULL) +
+  # Axe X en log10
   scale_x_log10(
     breaks = scales::trans_breaks("log10", function(x) 10^x),
     labels = scales::trans_format("log10", scales::math_format(10^.x))
   ) +
+  # Ligne horizontale à y=0
   geom_line(mapping = aes(x = x, y = 0), color = "black", linetype = "dashed") +
-  xlab("Log2 Base Mean")+
+  # Titres des axes
+  xlab("Mean of normalized counts") +
   ylab("Log2 fold change")
+
+dev.off()
+
+################################################################################
+#### Volcano plots
+################################################################################
+pdf(Volcano_plot_all)
+
+x = res$log2FoldChange
+y = -log10(pvalues)
+
+
+ggplot(data = cbind(x,y), 
+       mapping = aes(x,
+                     y, 
+                     color = ifelse(!is.na(pvalues) & pvalues > 0.05, 
+                                            "Non Significatif", 
+                                            "Significatif")))+
+  geom_point()+
+  scale_color_manual(values = c("Non Significatif" = "black", "Significatif" = "red"),
+                     name = NULL) +
+  xlab("Log2 fold change") +
+  ylab("-Log10 pvalues adjusted")
 
 dev.off()
 
@@ -87,6 +153,10 @@ dev.off()
 info = sessionInfo()
 
 
-writeLines(paste("Hello world\n R version : ", info$R.version$version.string,
-                 "\n DefSeq version : ", info$otherPkgs$DESeq2$Version),
-           con='/results/DESeq2/DESeq2_results.txt')
+writeLines(paste("R version : ", info$R.version$version.string,
+                 "\n DefSeq version : ", info$otherPkgs$DESeq2$Version,
+                 "\n\n Resultats Analyse differentielle tous les genes :
+                 \n genes diff : ", genes_diff,
+                 "\n genes up : ", genes_up,
+                 "\n genes down : ", genes_down),
+           con=res_txt)
